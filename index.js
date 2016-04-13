038b338ca1a85883282dd7798f929d5b6a19d729
Francis Brito <francis@instacarro.com>
2016-04-13 00:32:02 -0400

    Add support for parsing `sort` field.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 'use strict';
+const querystring = require('querystring');
+
+function parseQuery(qs) {
+  const rawQuery = querystring.parse(qs);
+  const parsedQuery = parseSortingFrom(rawQuery);
+
+  return parsedQuery;
+}
+
+function parseSortingFrom(q) {
+  const sortFields = q.sort.split(',');
+
+  return {
+    sort: sortFields.map(
+      sf => {
+        const sortFieldName = getFieldName(sf);
+        const sortFieldDirection = getSortDirection(sf);
+
+        return { [sortFieldName]: sortFieldDirection };
+      }
+    )
+    .reduce((query, f) => Object.assign({}, query, f), {}),
+  };
+}
+
+function getFieldName(sf) {
+  return sf[0] === '-' ? sf.slice(1) : sf;
+}
+
+function getSortDirection(sf) {
+  return sf[0] === '-' ? 'descending' : 'ascending';
+}
+
+module.exports = parseQuery;
--- tests.js
+++ tests.js
@@ -1,1 +1,20 @@
 'use strict';
+const test = require('tape');
+
+const parseQuery = require('./');
+
+test('it can parse `sort` query parameter', t => {
+  const queryString = 'sort=-foo,bar,-baz';
+  const expectedSorting = {
+    foo: 'descending',
+    bar: 'ascending',
+    baz: 'descending',
+  };
+  const query = parseQuery(queryString);
+  const actualSorting = query.sort;
+
+  t.ok(actualSorting, 'parsed query should have `sort` field.');
+  t.deepEqual(actualSorting, expectedSorting, '`sort` should equal expected sorting.');
+
+  t.end();
+});
